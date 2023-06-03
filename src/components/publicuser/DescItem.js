e3c28db0fa6ef7911a8a485f1a154650933344b2
Reza Raka Nugraha <rezaraka00@gmail.com>
2023-06-04 01:53:09 +0700

    fix: temporary hardcode image

--- src/components/publicuser/DescItem.js
+++ src/components/publicuser/DescItem.js
@@ -20,11 +20,11 @@ const DescItem = () => {
     id_pembeli: "",
     id_item: "",
     jumlah: "1",
-    total_harga: ""
+    total_harga: "",
   });
-  const [isAlertHijau, setIsAlertHijau] = useState(false)
-  const [isAlertMerah, setIsAlertMerah] = useState(false)
-  const [textAlert, setTextAlert] = useState('')
+  const [isAlertHijau, setIsAlertHijau] = useState(false);
+  const [isAlertMerah, setIsAlertMerah] = useState(false);
+  const [textAlert, setTextAlert] = useState("");
 
   useEffect(() => {
     const getItemById = async () => {
@@ -32,16 +32,15 @@ const DescItem = () => {
       setItemById(response.data);
     };
     getItemById();
-    
+
     window.scrollTo(0, 0);
     setDataItem((data) => ({ ...data, id_pembeli: id_pembeli, id_item: id }));
 
-    
     const getKeranjang = async () => {
-          const response = await axios.get(`${apiHost}keranjang/${id_pembeli}`);
-          setCekItemKeranjang(response.data);
-        };
-    if(id_pembeli !== undefined){
+      const response = await axios.get(`${apiHost}keranjang/${id_pembeli}`);
+      setCekItemKeranjang(response.data);
+    };
+    if (id_pembeli !== undefined) {
       getKeranjang();
     }
   }, []);
@@ -49,16 +48,16 @@ const DescItem = () => {
   const handleKeranjang = async (e) => {
     e.preventDefault();
     if (id_pembeli === undefined) {
-      setIsAlertMerah(true)
-      setTextAlert('Silahkan Login dahulu')
+      setIsAlertMerah(true);
+      setTextAlert("Silahkan Login dahulu");
     } else if (idItemKeranjang === itemById.id_item) {
-      setIsAlertMerah(true)
-      setTextAlert('Item sudah ada di keranjang')
+      setIsAlertMerah(true);
+      setTextAlert("Item sudah ada di keranjang");
     } else {
       try {
         await axios.post(`${apiHost}keranjang`, dataItem);
-        setIsAlertHijau(true)
-        setTextAlert('Item dimasukkan ke keranjang')
+        setIsAlertHijau(true);
+        setTextAlert("Item dimasukkan ke keranjang");
         window.location.reload();
         console.log("bisa kog");
       } catch (error) {
@@ -68,16 +67,16 @@ const DescItem = () => {
   };
 
   useEffect(() => {
-    cekItemKeranjang.map((data) => {
-      setIdItemKeranjang(data.id_item);
-    });
+    if (cekItemKeranjang && cekItemKeranjang?.length) {
+      cekItemKeranjang?.map((data) => {
+        setIdItemKeranjang(data.id_item);
+      });
+    }
   }, [handleKeranjang]);
 
   useEffect(() => {
-    setDataItem((data)=> ({...data,
-      total_harga : itemById.harga_item * 1
-    }))
-  },[itemById])
+    setDataItem((data) => ({ ...data, total_harga: itemById.harga_item * 1 }));
+  }, [itemById]);
 
   const formatUang = (number) => {
     return new Intl.NumberFormat("id-ID", {
@@ -95,114 +94,166 @@ const DescItem = () => {
   // }, 100)
   // console.log(idItemKeranjang)
   // console.log(cekItemKeranjang)
-  console.log(dataItem)
+  console.log(dataItem);
   console.log(itemById);
 
-    return ( 
-        <div className="descitem">
-            <div className="desc-item-con container ">
-                <div className="row pt-4" style={{ overflow:"hidden"}}>
-                    <div className="col d-flex justify-content-center">
-                        <div className="">
-                     
-                            <div id="carouselExampleIndicators" className="carousel slide" data-mdb-ride="carousel">
-                                <div className="carousel-inner">
-                                    <div className="carousel-item active">
-                                        <img src={KuraPlongo} className="carousel-foto-item d-block"/>
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={KuraPlongo} className="carousel-foto-item d-block"/>
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={KuraPlongo} className="carousel-foto-item d-block"/>
-                                    </div>
-                                    <div className="carousel-item">
-                                        <img src={KuraPlongo} className="carousel-foto-item d-block"/>
-                                    </div>
-                                </div>
-                                  <button
-                                    className="carousel-control-prev"
-                                    type="button"
-                                    data-mdb-target="#carouselExampleIndicators"
-                                    data-mdb-slide="prev"
-                                  >
-                                    <span
-                                      className="carousel-control-prev-icon"
-                                      aria-hidden="true"
-                                    ></span>
-                                    <span className="visually-hidden">Previous</span>
-                                  </button>
-                                  <button
-                                    className="carousel-control-next"
-                                    type="button"
-                                    data-mdb-target="#carouselExampleIndicators"
-                                    data-mdb-slide="next"
-                                  >
-                                    <span
-                                      className="carousel-control-next-icon"
-                                      aria-hidden="true"
-                                    ></span>
-                                    <span className="visually-hidden">Next</span>
-                                  </button>
-                                <div className="carousel-indicators-desc d-flex justify-content-center">
-                                    <button className='thumbnail-carousel-desc active' data-target="#carouselExampleIndicators" data-slide-to="0">
-                                        <img src={KuraPlongo} className="d-block w-100"/>
-                                    </button>
-                                    <button className='thumbnail-carousel-desc' data-target="#carouselExampleIndicators" data-slide-to="1">
-                                        <img src={KuraPlongo} className="d-block w-100"/>
-                                    </button>
-                                    <button className='thumbnail-carousel-desc' data-target="#carouselExampleIndicators" data-slide-to="2">
-                                        <img src={KuraPlongo} className="d-block w-100"/>
-                                    </button>
-                                    <button className='thumbnail-carousel-desc' data-target="#carouselExampleIndicators" data-slide-to="3">
-                                        <img src={KuraPlongo} className="d-block w-100"/>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col ">
-                      <div className="desc-kanan px-3 ">
-                        <div className="py-2 container">
-                          <p className="text-item-name-desc">{itemById.nama_item}</p>
-                          <p className="text-item-price-desc">{formatUang(itemById.harga_item).replace(/\,00/g, "")}</p>
-                          <div className="d-flex" style={{height:"30px"}}>
-                            <p className="text-item-stok">Stok :</p>
-                            <p className="px-1 text-item-stok">{itemById.stok_item}</p>
-                          </div>
-                        </div>
-
-                        <hr />
-                        <div className="desc-item ">
-                          <p className="text-desc-item-desc">{itemById.deskripsi_item}</p>
-                        </div>
-
-                        <hr />
-                        <div className="row desc-item-toko ">
-                          <div className="col-2  d-flex justify-content-end">
-                            <div className="desc-foto-toko-item">
-                              <img className="" src="" />
-                            </div>
-                          </div>
-                          <div className="col p-1">
-                            <p className="text-name-toko-item">{itemById.nama_toko}</p>
-                            <p className="text-name-alamt-toko">{itemById.alamat_toko}</p>
-                          </div>
-                        </div>
-                        <hr />
-                          <div className="d-flex justify-content-end align-items-end" style={{height:"50px"}} >
-                            <button className='but-cart' onClick={handleKeranjang}>Tambah ke Keranjang</button>
-                          </div>
-                        </div>
-                                  
-                            </div>
-                          </div>
-
+  return (
+    <div className="descitem">
+      <div className="desc-item-con container ">
+        <div className="row pt-4" style={{ overflow: "hidden" }}>
+          <div className="col d-flex justify-content-center">
+            <div className="">
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide"
+                data-mdb-ride="carousel"
+              >
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src={KuraPlongo}
+                      className="carousel-foto-item d-block"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={KuraPlongo}
+                      className="carousel-foto-item d-block"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={KuraPlongo}
+                      className="carousel-foto-item d-block"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={KuraPlongo}
+                      className="carousel-foto-item d-block"
+                    />
+                  </div>
+                </div>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  data-mdb-target="#carouselExampleIndicators"
+                  data-mdb-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  data-mdb-target="#carouselExampleIndicators"
+                  data-mdb-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
+                <div className="carousel-indicators-desc d-flex justify-content-center">
+                  <button
+                    className="thumbnail-carousel-desc active"
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                  >
+                    <img src={KuraPlongo} className="d-block w-100" />
+                  </button>
+                  <button
+                    className="thumbnail-carousel-desc"
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="1"
+                  >
+                    <img src={KuraPlongo} className="d-block w-100" />
+                  </button>
+                  <button
+                    className="thumbnail-carousel-desc"
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="2"
+                  >
+                    <img src={KuraPlongo} className="d-block w-100" />
+                  </button>
+                  <button
+                    className="thumbnail-carousel-desc"
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="3"
+                  >
+                    <img src={KuraPlongo} className="d-block w-100" />
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="d-flex justify-content-center" >
-              {isAlertHijau ? <AlertHijau textAlert={textAlert} isAlert={isAlertHijau} setIsAlert={setIsAlertHijau}/> : <div></div>}
-              {isAlertMerah ? <AlertMerah textAlert={textAlert} isAlert={isAlertMerah} setIsAlert={setIsAlertMerah}/> : <div></div>}
           </div>
+          <div className="col ">
+            <div className="desc-kanan px-3 ">
+              <div className="py-2 container">
+                <p className="text-item-name-desc">{itemById.nama_item}</p>
+                <p className="text-item-price-desc">
+                  {formatUang(itemById.harga_item).replace(/\,00/g, "")}
+                </p>
+                <div className="d-flex" style={{ height: "30px" }}>
+                  <p className="text-item-stok">Stok :</p>
+                  <p className="px-1 text-item-stok">{itemById.stok_item}</p>
+                </div>
+              </div>
+
+              <hr />
+              <div className="desc-item ">
+                <p className="text-desc-item-desc">{itemById.deskripsi_item}</p>
+              </div>
+
+              <hr />
+              <div className="row desc-item-toko ">
+                <div className="col-2  d-flex justify-content-end">
+                  <div className="desc-foto-toko-item">
+                    <img className="" src="" />
+                  </div>
+                </div>
+                <div className="col p-1">
+                  <p className="text-name-toko-item">{itemById.nama_toko}</p>
+                  <p className="text-name-alamt-toko">{itemById.alamat_toko}</p>
+                </div>
+              </div>
+              <hr />
+              <div
+                className="d-flex justify-content-end align-items-end"
+                style={{ height: "50px" }}
+              >
+                <button className="but-cart" onClick={handleKeranjang}>
+                  Tambah ke Keranjang
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        {isAlertHijau ? (
+          <AlertHijau
+            textAlert={textAlert}
+            isAlert={isAlertHijau}
+            setIsAlert={setIsAlertHijau}
+          />
+        ) : (
+          <div></div>
+        )}
+        {isAlertMerah ? (
+          <AlertMerah
+            textAlert={textAlert}
+            isAlert={isAlertMerah}
+            setIsAlert={setIsAlertMerah}
+          />
+        ) : (
+          <div></div>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/publicuser/Items.js
+++ src/components/publicuser/Items.js
@@ -11,9 +11,8 @@ const Items = () => {
 
   useEffect(() => {
     const dataDB = async () => {
-      const response = await axios.get(
-        `${apiHost}item?search=${state.search}`
-      );
+      const response = await axios.get(`${apiHost}item?search=${state.search}`);
+      console.log("response", response);
       setDatum(response.data);
       // console.log(response)
     };
@@ -29,30 +28,50 @@ const Items = () => {
 
   console.log(datum);
   return (
-    <div className="bg-items my-5 d-flex justify-content-center" style={{ width: "100%" }}>
+    <div
+      className="bg-items my-5 d-flex justify-content-center"
+      style={{ width: "100%" }}
+    >
       <div className="items p-4 d-flex justify-content-center">
         <div className="row gap-4 d-flex justify-content-center align-items-center row-cols-5">
-            {datum.map((item) => {
-                console.log(item.gambar)
-                  return(
-                    <div className="item m-2" key={item.id_item} style={{cursor:"pointer", padding:'0px'}}>
-                      <Link to={`/item/${item.id_item}`} style={{ textDecoration:"none", color:"black"}}>
-                        <div className="img-thumbnail-item" >
-                          <img className='item-image' src={item.gambar} alt=""/>
-                        </div>
-                        <div className="item-thumb-desc item-name mt-1 mx-2">
-                          <p className='text-item-name'>{item.nama_item}</p>
-                        </div>
-                        <br />
-                        <div className="item-thumb-desc item-price mx-2">
-                          <p className='text-item-price'>{formatUang(item.harga_item).replace(/,00/g, '')}</p>
-                        </div>
-                        <div className="item-thumb-desc mx-2 ">
-                          <p className='text-item-toko d-flex align-items-end pb-1'>{item.nama_toko}</p>
-                        </div>
-                      </Link>
-                    </div>
-                  )
+          {datum.map((item) => {
+            console.log("gambar", item.gambar);
+            return (
+              <div
+                className="item m-2"
+                key={item.id_item}
+                style={{ cursor: "pointer", padding: "0px" }}
+              >
+                <Link
+                  to={`/item/${item.id_item}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  <div className="img-thumbnail-item">
+                    <img
+                      className="item-image"
+                      src={
+                        "http://localhost:3311/1b84a10e4350de424f0620f56fd5c912.png"
+                      }
+                      alt=""
+                    />
+                  </div>
+                  <div className="item-thumb-desc item-name mt-1 mx-2">
+                    <p className="text-item-name">{item.nama_item}</p>
+                  </div>
+                  <br />
+                  <div className="item-thumb-desc item-price mx-2">
+                    <p className="text-item-price">
+                      {formatUang(item.harga_item).replace(/,00/g, "")}
+                    </p>
+                  </div>
+                  <div className="item-thumb-desc mx-2 ">
+                    <p className="text-item-toko d-flex align-items-end pb-1">
+                      {item.nama_toko}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            );
           })}
         </div>
       </div>
