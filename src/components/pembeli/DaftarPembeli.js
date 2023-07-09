d3537add5285916d74709492970e27dad2ef6e13
Galangyp <galangy11@gmail.com>
2023-07-09 15:31:15 +0700

    fix bugs

--- src/components/pembeli/DaftarPembeli.js
+++ src/components/pembeli/DaftarPembeli.js
@@ -1,155 +1,197 @@
-import axios from 'axios';
-import apiHost from '../../constants/apiHost'
-import './daftarpembeli.css';
-import { useEffect, useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import axios from "axios";
+import apiHost from "../../constants/apiHost";
+import "./daftarpembeli.css";
+import { useEffect, useState } from "react";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+import { useNavigate, Link } from "react-router-dom";
 
-import Alert from '../AlertMerah';
+import Alert from "../AlertMerah";
 
 const DaftarPembeli = () => {
+  const navigate = useNavigate();
+  const [dataInput, setDataInput] = useState({
+    email: "",
+    username: "",
+    password: "",
+    alamat: "",
+    foto_profil: "",
+    no_rek_pembeli: null,
+    level: "Pembeli",
+  });
+  const [isAlert, setIsAlert] = useState(false);
+  const [textAlert, setTextAlert] = useState("");
+  const [dataPembeli, setDataPembeli] = useState([]);
+  const [typePassword, setTypePassword] = useState("password");
+  const [isHiddenPass, setIsHiddenPass] = useState(false);
 
-    const navigate = useNavigate()
-    const [dataInput, setDataInput] = useState({
-        email: "",
-        username: "",
-        password: "",
-        alamat:"",
-        foto_profil:"",
-        no_rek_pembeli:null,
-        level: "Pembeli"
-    })
-    const [isAlert, setIsAlert] = useState(false)
-    const [textAlert, setTextAlert] = useState('')
-    const [dataPembeli, setDataPembeli] = useState([])
+  useEffect(() => {
+    const getDataPembeli = async () => {
+      const response = await axios.get(`${apiHost}pembeli`);
+      setDataPembeli(response.data);
+    };
 
-    useEffect(()=>{
-        const getDataPembeli = async() =>{
-            const response = await axios.get(`${apiHost}pembeli`)
-            setDataPembeli(response.data)
-        }
+    getDataPembeli();
+  }, []);
 
-        getDataPembeli()
-    },[])
+  console.log(dataPembeli);
+  const handleInput = (e) => {
+    setDataInput((data) => ({ ...data, [e.target.id]: e.target.value }));
+  };
 
-    console.log(dataPembeli)
-    const handleInput = (e) =>{
-        setDataInput((data) => ({...data, 
-            [e.target.id] : e.target.value
-        }))
+  const handleDaftarPembeli = async (e) => {
+    e.preventDefault();
+
+    if (dataInput.email === "") {
+      setIsAlert(true);
+      setTextAlert("Email tidak boleh kosong!");
     }
-    
-    const handleDaftarPembeli = async(e) =>{
-        e.preventDefault()
-
-        if(dataInput.email === ""){
-            setIsAlert(true)
-            setTextAlert('Email tidak boleh kosong!')
-        }
-        // else if(dataPembeli.map((data)=>{return(dataInput.email === data.email)})){
-        //     setIsAlert(true)
-        //     setTextAlert('Email sudah terdaftar!')
-        // }
-        else if(dataInput.password === ""){
-            setIsAlert(true)
-            setTextAlert('Password tidak boleh kosong!')
-        }else if(dataInput.password.length < 8){
-            setIsAlert(true)
-            setTextAlert('Password harus lebih dari 8 karakter')
-        }else if(dataInput.username === ""){
-            setIsAlert(true)
-            setTextAlert('Username tidak boleh kosong!')
-        }else{
-                    try {
-                        await axios.post(`${apiHost}pembeli`, dataInput);
-                        navigate('/login');
-                        console.log('bisa kog')
-                    } catch (error) {
-                        console.log('eror bang gabisa input')
-                    }
-                }
+    // else if(dataPembeli.map((data)=>{return(dataInput.email === data.email)})){
+    //     setIsAlert(true)
+    //     setTextAlert('Email sudah terdaftar!')
+    // }
+    else if (dataInput.password === "") {
+      setIsAlert(true);
+      setTextAlert("Password tidak boleh kosong!");
+    } else if (dataInput.password.length < 8) {
+      setIsAlert(true);
+      setTextAlert("Password harus lebih dari 8 karakter");
+    } else if (dataInput.username === "") {
+      setIsAlert(true);
+      setTextAlert("Username tidak boleh kosong!");
+    } else {
+      try {
+        await axios.post(`${apiHost}pembeli`, dataInput);
+        navigate("/login");
+        console.log("bisa kog");
+      } catch (error) {
+        console.log("eror bang gabisa input");
+      }
     }
+  };
 
-    console.log(dataInput)
-    return ( 
-        <div className="daftar-pembeli">
-            <div className="daftar-pembeli-con container d-flex justify-content-center align-items-center">
-                <div className="form-daftar-pembeli d-grid row d-flex justify-content-center">
+  const handleHiddenPass = (e) => {
+    e.preventDefault();
+    setIsHiddenPass(false);
+    setTypePassword("password");
+  };
 
-                    <div className="form-head-pembeli my-0 d-flex justify-content-center row ">
-                        <h2 className='text-registrasi-pembeli d-flex justify-content-center align-items-center'>Registrasi</h2>
-                    </div>
+  const handleShowPass = (e) => {
+    e.preventDefault();
+    setIsHiddenPass(true);
+    setTypePassword("text");
+  };
 
-                    <div className="form-body-pembeli mb-2 d-flex justify-content-center row">
-                        <div className="row d-flex justify-content-center">
-                            <label htmlFor="email"  id='label-input'>Email</label>
-                            <input
-                                className='input-text'
-                                type="text"
-                                required
-                                placeholder='Masukan email anda...'
-                                id="email"
-                                value={dataInput.email}
-                                onChange={handleInput}
-                            />
-                        </div>
+  return (
+    <div className="daftar-pembeli">
+      <div className="daftar-pembeli-con container d-flex justify-content-center align-items-center">
+        <div className="form-daftar-pembeli d-grid row d-flex justify-content-center">
+          <div className="form-head-pembeli my-0 d-flex justify-content-center row ">
+            <h2 className="text-registrasi-pembeli d-flex justify-content-center align-items-center">
+              Registrasi
+            </h2>
+          </div>
 
-                        <div className="row d-flex justify-content-center">
-                            <label htmlFor="password" id='label-input'>Password</label>
-                            <input 
-                                className='input-text'
-                                type="password" 
-                                required
-                                placeholder='Masukan password anda...'
-                                id="password"
-                                value={dataInput.password}
-                                onChange={handleInput}
-                            />  
-                        </div>
-
-                        <div className="row d-flex justify-content-center">
-                            <label htmlFor="username"  id='label-input'>Username</label>
-                            <input
-                                className='input-text'
-                                type="text"
-                                required
-                                placeholder='Masukan username anda...'
-                                id="username"
-                                value={dataInput.username}
-                                onChange={handleInput}
-                            />
-                        </div>
-
-                    </div>
-
-                    <div className="form-footer-pembeli my-2 row d-flex justify-content-center align-items-end">
-                        <div className="col d-flex justify-content-start">
-                            <div className="but-beranda-daftar-pembeli" onClick={()=>navigate('/')}>
-                                <p className='text-but-beranda-daftar-pembeli d-flex justify-content-center align-items-center'>Beranda</p>
-                            </div>
-                        </div>
-                        <div className="col d-flex justify-content-end">
-                            <div className="but-next-daftar-pembeli" onClick={handleDaftarPembeli}>
-                                <p className='text-but-next-daftar-pembeli d-flex justify-content-center align-items-center' >Daftar</p>
-                            </div>
-                        </div>
-                        <div className="row d-flex align-items-end" style={{height:"30px"}}>
-                                        <div className="col d-flex justify-content-center align-items-center">
-                                            <p className='text-daftar-login d-flex justify-content-start align-items-center'>Ingin bergabung sebagai penjual?</p>
-                                        </div>
-                                        <div className="col-2 p-0 d-flex justify-content-start align-items-center">
-                                            <Link to={'/daftar-penjual'} style={{ textDecoration:"none"}}>
-                                                <p className='but-daftar-login d-flex justify-content-start align-items-center' >Gabung!</p>
-                                            </Link>
-                                        </div>
-                                    </div>
-                    </div>
-               </div>
+          <div className="form-body-pembeli mb-2 d-flex justify-content-center row">
+            <div className="row d-flex justify-content-center">
+              <label htmlFor="email" id="label-input">
+                Email
+              </label>
+              <input
+                className="input-text"
+                type="text"
+                required
+                placeholder="Masukan email anda..."
+                id="email"
+                value={dataInput.email}
+                onChange={handleInput}
+              />
             </div>
-            <div className="d-flex justify-content-center">
-                {isAlert ? <Alert textAlert={textAlert} isAlert={isAlert} setIsAlert={setIsAlert}/> : <div></div>}
+
+            <div className="row d-flex justify-content-center">
+              <label htmlFor="password" id="label-input">
+                Password
+              </label>
+              <input
+                className="input-text mx-2"
+                type={typePassword}
+                required
+                placeholder="Masukan password anda..."
+                id="password"
+                value={dataInput.password}
+                onChange={handleInput}
+              />
+              <div className="lihat-password">
+                {isHiddenPass ? (
+                  <BsEyeFill
+                    size="1.2em"
+                    color="#0E8388"
+                    onClick={handleHiddenPass}
+                    style={{ cursor: "pointer" }}
+                  />
+                ) : (
+                  <BsEyeSlashFill
+                    size="1.2em"
+                    color="#0E8388"
+                    onClick={handleShowPass}
+                    style={{ cursor: "pointer" }}
+                  />
+                )}
+              </div>
             </div>
+
+            <div className="row d-flex justify-content-center">
+              <label htmlFor="username" id="label-input">
+                Username
+              </label>
+              <input
+                className="input-text"
+                type="text"
+                required
+                placeholder="Masukan username anda..."
+                id="username"
+                value={dataInput.username}
+                onChange={handleInput}
+              />
+            </div>
+          </div>
+
+          <div className="form-footer-pembeli my-2 row d-flex justify-content-center align-items-end">
+            <div className="col d-flex justify-content-start">
+              <div
+                className="but-beranda-daftar-pembeli"
+                onClick={() => navigate("/")}
+              >
+                <p className="text-but-beranda-daftar-pembeli d-flex justify-content-center align-items-center">
+                  Beranda
+                </p>
+              </div>
+            </div>
+            <div className="col d-flex justify-content-end">
+              <div
+                className="but-next-daftar-pembeli"
+                onClick={handleDaftarPembeli}
+              >
+                <p className="text-but-next-daftar-pembeli d-flex justify-content-center align-items-center">
+                  Daftar
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+      <div className="d-flex justify-content-center">
+        {isAlert ? (
+          <Alert
+            textAlert={textAlert}
+            isAlert={isAlert}
+            setIsAlert={setIsAlert}
+          />
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default DaftarPembeli;
--- src/constants/apiHost.js
+++ src/constants/apiHost.js
@@ -1,2 +1,2 @@
-export default "https://web-toko-skw-be.vercel.app/";
-// export default "http://localhost:3311/";
+// export default "https://web-toko-skw-be.vercel.app/";
+export default "http://localhost:3311/";
