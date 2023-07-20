f8e8c6613d53f86abcf700142a40128bed90150b
Galangyp <galangy11@gmail.com>
2023-07-20 22:59:13 +0700

    ubah footer

--- src/components/publicuser/Footer.js
+++ src/components/publicuser/Footer.js
@@ -1,53 +1,49 @@
 import "./footer.css";
-import kucing from "../image/kucinglowpixel.jpg";
-import kura from "../image/kuraplongo.jpg";
+import logo from "../image/logo-skw.png";
+import sman3 from "../image/sman3.png";
+import upi from "../image/upi.png";
+import psti from "../image/psti.png";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
+import {BiLogoInstagram} from "react-icons/bi";
 
-import { BsInstagram } from "react-icons/bs";
-import { BsTiktok } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div className="footer p-5">
       <div className="row" style={{ height: "100%" }}>
-        <div className="col border">
+        <div className="col">
           <div className="row"></div>
-          <h2 className="footer-text">Tentang</h2>
-
-          <p className="footer-text">
-            Web Toko SKW merupakan sebuah wadah untuk begitu yang mana akan
-            mempermudah begituan antar begituan. Sistem ini merupakan salah satu
-            satu layanan akademik untuk mahasiswa UPI yang memuat seluruh
-            aktifitas utama administrasi akademik yang antara lain berisi
-            fitur-fitur manajemen DIM (Data Induk Mahasiswa), pengajuan IRS dan
-            PRS, informasi Kalender Akademik, informasi Jadwal Kuliah, riwayat
-            status akademis, riwayat capaian nilai dan transkip nilai sementara,
-            termasuk didalamnya berita akademik.
+          
+          <div className="row">
+            <img className="logo" src={logo} alt="" />
+          </div>
+          <p className="footer-text-1">
+          Web Toko SKW merupakan sebuah wadah kegiatan wirausaha siswa SMAN 3 Purwakarta.
           </p>
         </div>
 
-        <div className="col border">
-          <div className="row border" style={{ height: "50%" }}>
+        <div className="col">
+          <div className="row" style={{ height: "50%" }}>
             <h2 className="footer-text">Hubungi kami</h2>
-          </div>
-          <div className="row border" style={{ height: "50%" }}>
-            <h2 className="footer-text">FAQ</h2>
+              <div className="hubungi-1 d-flex justify-content-left" >         
+               <MdOutlineMailOutline color="#E7F6F2" size="30px" className="mx-2" />sman3pwk@gmail.com   
+               </div>
+               <div className="hubungi-2 d-flex justify-content-left" >         
+               <FaWhatsapp color="#E7F6F2" size="30px" className="mx-2" />08155146001   
+               </div>
+               <div className="hubungi-1 d-flex justify-content-left" >         
+               <BiLogoInstagram color="#E7F6F2" size="30px" className="mx-2" />@skwsmanti_   
+               </div>
           </div>
         </div>
-
-        <div className="col border" style={{ height: "100%" }}>
-          <div className="row my-2 d-flex justify-content-end">
-            <img className="sponsor" src={kucing} alt="" />
-          </div>
-
-          <div className="row my-2 d-flex justify-content-end">
-            <img className="sponsor" src={kura} alt="" />
-          </div>
-
-          <div className="row my-5 ">
-            <div className="col d-flex justify-content-end">
-              <BsInstagram color="#E7F6F2" size="25px" className="mx-2" />
-              <BsTiktok color="#E7F6F2" size="25px" className="mx-2" />
-            </div>
+ 
+        <div className="col" style={{ height: "100%" }}>
+         <h2 className="footer-text">Powered By</h2>
+          <div className="row my-2 d-flex justify-content-left">
+            <img className="sponsor mx-2" src={sman3} alt="" />
+            <img className="sponsor mx-2" src={upi} alt="" />
+            <img className="sponsor mx-2" src={psti} alt="" />
           </div>
         </div>
       </div>
